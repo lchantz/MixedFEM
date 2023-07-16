a17058b4a2f88373d3ec0b808047b50d073f1a91
lchantz <lambroschantz@gmail.com>
2023-07-16 20:52:51 +0300

    WorkingVersion

--- CahnHilliard/CahnHilliard.f90
+++ CahnHilliard/CahnHilliard.f90
@@ -1,5 +1,5 @@
 !>******************************************************************************
-!>    The form
+!>    The form of the Cahn Hilliard equation is
 !>******************************************************************************
 program CahnHilliard
    use, intrinsic :: iso_fortran_env
@@ -36,7 +36,7 @@ program CahnHilliard
    !> Derivative of the non-linear term
    REAL(real64) DP(NNmax,NNmax,Tmax)
 
-   INTEGER i,j,timepoint
+   INTEGER i,timepoint
 
    OPEN(unit=2,file='OrderParameter.dat')
 
@@ -54,7 +54,7 @@ program CahnHilliard
       CALL NewtonProcedure(x,y,nop,t,timepoint,c,w,S,STIFF,MASS,P,DP)
       DO i=1,NNmax
 
-         WRITE(2,*) t(timepoint), i, c(i,timepoint)
+         WRITE(2,'(2x,E16.9,1x,E16.9,1x,E16.9,1x,E16.9)') t(timepoint), x(i),y(i), c(i,timepoint) 
 
       ENDDO
    ENDDO
@@ -83,12 +83,12 @@ SUBROUTINE SpaceDiscretization(x,y)
    REAL(real64) deltax,deltay
    INTEGER i,j,NNode
 
-   xorigin=1.
-   xlast=5.
+   xorigin=0.
+   xlast=1.
    deltax=(xlast-xorigin)/2*NELx
 
-   yorigin=1.
-   ylast=5.
+   yorigin=0.
+   ylast=1.
    deltay=(ylast-yorigin)/2*NELy
 
    !> Setting the coordinates of each node
@@ -173,7 +173,7 @@ SUBROUTINE TimeDiscretization(t)
 END SUBROUTINE TimeDiscretization
 
 !>******************************************************************************
- !> Initial&BoundaryConditions returns the initial solution (w,u) to be used by
+ !> Initial&BoundaryConditions returns the initial solution (w_0,u_0) to be used by
  !> the Newton procedure.
  !> -------------
  !> Params:
@@ -202,7 +202,8 @@ SUBROUTINE InitialBoundaryConditions(x,y,nop, t, c, w)
    LOGICAL:: isInitialTime
    INTEGER i,timepoint
 
-   !Impose Boundary and initial conditions on u, w
+   !Impose Periodic Boundary and initial conditions on c, w
+   !can be redefined using the nop correspondence, so it appears on argument list
    DO i=1,NNmax
       isBoundaryPoint = ((x(i) == 0.) .or. (x(i) == 1.) .or. (y(i) == 0. .or. y(i) == 1.))
       DO timepoint=1,Tmax
@@ -223,18 +224,165 @@ SUBROUTINE InitialBoundaryConditions(x,y,nop, t, c, w)
 END SUBROUTINE InitialBoundaryConditions
 
 
- !>******************************************************************************
+ !>***************************************************************************************
  !> WeakFormulation initializes and calculates the system matrices and the nonlinear term
- !> imposes the Boundary conditions
+ !> taking into account the Periodic Boundary conditions
  !>
  !> -------------
  !> Params:
  !> -------------
- !> x:                   Space coordinates
- !> A,K:                 System matrices
- !> ngl:                 global coordinates in space
- !>******************************************************************************
+ !> x,y:                   Space coordinates
+ !> STIFF,MASS,P:          System matrices
+ !> ngl:                   ngl --> node global numbering
+ !>**************************************************************************************
 
+SUBROUTINE WeakFormulation(x,y,STIFF,MASS,P,nop)
+   use, intrinsic :: iso_fortran_env
+   IMPLICIT NONE
+   INCLUDE 'CHparameters.inc'
+
+   !> Space coordinates
+   REAL(real64), INTENT(IN) :: x(NNmax),y(NNmax)
+
+   !> System matrices
+   !> STIFF - Stiffness
+   !> MASS - Mass
+   !> P - Non linear term from double well potential c^3-c
+   REAL(real64) STIFF(NNmax,NNmax)
+   REAL(real64) MASS(NNmax,NNmax)
+   REAL(real64) P(NNmax,Tmax)
+
+   !> Local to global space numbering / ngl --> node global numbering
+   INTEGER nop(NELmax,NNref)
+
+   INTEGER i,element
+   LOGICAL:: isBoundaryPoint
+
+   STIFF=0.
+   MASS=0.
+   P=0.
+
+   DO element=1,NELmax
+      CALL GaussianQuadrature(element,x,y,STIFF,MASS,nop)
+   ENDDO
+
+   !> Impose Periodic Boundary conditions on STIFF,MASS,P
+   !> A,P set to zero to be eliminated for generating equations on c,w accuratelly
+   !> Page 5 of Notes for explanation
+   DO i=1,NNmax
+      isBoundaryPoint = ((x(i) == 0.) .or. (x(i) == 1.) .or. (y(i) == 0. .or. y(i) == 1.))
+      IF (isBoundaryPoint) THEN
+         MASS(i,:) = 0.
+         MASS(i,i) = 1.
+         P(i,:) = 0.
+         STIFF(i,:) = 0.
+      ENDIF
+   ENDDO      
+  
+
+
+
+END SUBROUTINE WeakFormulation
+
+!>******************************************************************************
+!> GaussianQuadrature numerical method for evaluation of integrals appearing in
+!> system matrices.
+!> -------------
+!> Params:
+!> -------------
+!> element:             the current element
+!> x,y:                 spatial coordinates
+!> STIFF,MASS:          System matrices
+!> ngl:                 Local to global space numbering
+!>******************************************************************************
+
+SUBROUTINE GaussianQuadrature(element,x,y,STIFF,MASS,nop)
+   use, intrinsic :: iso_fortran_env
+   IMPLICIT NONE
+   INCLUDE 'CHparameters.inc'
+
+
+   !> Current element
+   INTEGER element
+
+   !> Space coordinates !xpt,ypt
+   REAL(real64), INTENT(IN) :: x(NNmax),y(NNmax)
+
+   !> System matrices
+   !> STIFF - Stiffness
+   !> MASS - Mass
+   REAL(real64) STIFF(NNmax,NNmax)
+   REAL(real64) MASS(NNmax,NNmax)
+  
+
+   !> Local to global space numbering
+   INTEGER nop(NELmax,NNref)
+   INTEGER ngl(NNref)
+
+   REAL(real64) phi(NNref), tphx(NNref), tphy(NNref),phic(NNref),phie(NNref)
+   REAL(real64) gp(3), gw(3)
+   REAL(real64) Xcomputational,Xc,Xe,Ycomputational,Yc,Ye,dett
+
+   
+   INTEGER i,j,k,l,m,n
+   
+
+
+   gw  =(/0.27777777777778, 0.444444444444, 0.27777777777778/)
+   gp =(/0.1127016654    , 0.5           , 0.8872983346    /)
+
+   DO i = 1,NNref
+      ngl(i) = nop(element,i)
+   ENDDO  
+
+   phi=0.
+   phic=0.
+   phie=0.
+   tphx=0.
+   tphy=0.
+   ! Loop over qauss points
+   DO j = 1,3
+      DO k = 1,3
+
+      ! TODO: check test func | tphy,tphx,dett -> nan
+      call TestFunctions(gp(j),gp(k),phi,phic,phie)
+
+      ! Defines the computational domain coordinates and the 2-dimensional Jacobian dett
+      Xcomputational=0.
+      Xc=0.
+      Xe=0.
+      Ycomputational=0.
+      Yc=0.
+      Ye=0.
+      DO n=1,NNref
+         Xcomputational= Xcomputational + x(ngl(n)) * phi(n)
+         Xc= Xc + x(ngl(n)) * phic(n)
+         Xe= Xe + x(ngl(n)) * phie(n)
+         Ycomputational= Ycomputational + y(ngl(n)) * phi(n)
+         Yc= Yc + y(ngl(n)) * phic(n)
+         Ye= Ye + y(ngl(n)) * phie(n)
+      ENDDO
+      dett=Xc*Ye-Xe*Yc
+
+      DO i=1,NNref
+         tphx(i)=(Ye*phic(i)-Yc*phie(i))/dett
+         tphy(i)=(Xc*phie(i)-Xe*phic(i))/dett
+      ENDDO
+
+   
+
+
+
+      DO l=1,NNref
+         DO m=1,NNref
+            STIFF(ngl(l),ngl(m)) = STIFF(ngl(l),ngl(m)) + gw(j)*gw(k)*dett*(tphx(l)*tphx(m)+tphy(l)*tphy(m))
+            MASS(ngl(l),ngl(m)) = MASS(ngl(l),ngl(m)) + gw(j)*gw(k)*dett*(phi(l)*phi(m))
+         ENDDO
+      ENDDO
+   ENDDO !End of loop over gauss points
+   ENDDO !End of loop over gauss points
+
+END SUBROUTINE GaussianQuadrature
 
 
 
@@ -249,12 +397,12 @@ END SUBROUTINE InitialBoundaryConditions
  !> -------------
  !> Params:
  !> -------------
- !> Y:                   Newton solution vector
-
+ !> S:                   Newton solution vector
+ !> c:                   Order parameter   
  !> w:                   Auxiliary vector
- !> A,K:                 System matrices
- !> f:                   Non-linear term of the weak formulation
- !> Df:                  Derivative of the non-linear term of the weak formulation
+ !> STIFF,MASS:                 System matrices
+ !> P:                   Non-linear term of the weak formulation
+ !> DP:                  Derivative of the non-linear term of the weak formulation
  !>******************************************************************************
 SUBROUTINE NewtonProcedure(x,y,nop,t,timepoint,c,w,S,STIFF,MASS,P,DP)
    use, intrinsic :: iso_fortran_env
@@ -283,7 +431,6 @@ SUBROUTINE NewtonProcedure(x,y,nop,t,timepoint,c,w,S,STIFF,MASS,P,DP)
    !> System matrices
    !> STIFF - Stiffness
    !> MASS - Mass
-   !> P - Non linear advective term from double well potential c^3-c
    REAL(real64) STIFF(NNmax,NNmax)
    REAL(real64) MASS(NNmax,NNmax)
    !> Non-linear term of the weak formulation
@@ -315,7 +462,8 @@ SUBROUTINE NewtonProcedure(x,y,nop,t,timepoint,c,w,S,STIFF,MASS,P,DP)
    S = S * 0.99_real64
 
 
-   !> Imposing the boundary conditions on the stationary solution vector S
+   !> Imposing the Periodic (essential)
+   !> boundary conditions on the stationary solution vector S
    DomainBoundary=0.
    DO i=1,NNy
       DomainBoundary(i)=1.
@@ -342,7 +490,7 @@ SUBROUTINE NewtonProcedure(x,y,nop,t,timepoint,c,w,S,STIFF,MASS,P,DP)
    ntrial = 100
 
    ! Solve
-   do trial=1,ntrial
+   DO trial=1,ntrial
 
       ! Form non linear term
       DO NEL=1,NELmax
@@ -370,9 +518,194 @@ SUBROUTINE NewtonProcedure(x,y,nop,t,timepoint,c,w,S,STIFF,MASS,P,DP)
          c(:, MIN(timePoint+1, Tmax)) = S(NNmax+1:2*NNmax)
          return
       ENDIF
-   enddo
+   ENDDO
 
 END SUBROUTINE
+
+!>******************************************************************************
+!> NonlinearTerm evaluates the entries of the P(c,t) operator and the DP(c,t) matrix
+!> at a fixed time step.
+!> -------------
+!> Params:
+!> -------------
+!> x,y:                 spatial coordinates
+!> NEL:                 current element
+!> nop:                 local to global node numbering
+!> timePoint:           time point
+!> c:                   order parameter
+!> P:                   Non-linear term of the weak formulation
+!> DP:                  Derivative of the non-linear term
+!>******************************************************************************
+
+
+SUBROUTINE NonlinearTerm(x,y,NEL,nop,timepoint,c,P,DP)
+   use, intrinsic :: iso_fortran_env
+   IMPLICIT NONE
+   INCLUDE 'CHparameters.inc'
+
+  !> Current element
+   INTEGER NEL
+
+
+   !> Space coordinates !xpt,ypt
+   REAL(real64), INTENT(IN) :: x(NNmax),y(NNmax)
+
+   !> timepoint
+   INTEGER timepoint
+
+   !> Order parameter - c - of the Cahn Hilliard Equation
+   REAL(real64) c(NNmax,Tmax)
+
+   !> non linear term
+   REAL(real64) P(NNmax,Tmax)
+   !> Derivative of the non-linear term
+   REAL(real64) DP(NNmax,NNmax,Tmax)
+
+
+   !> Local to global space numbering
+   INTEGER nop(NELmax,NNref)
+   INTEGER ngl(NNref)
+
+   REAL(real64) phi(NNref), tphx(NNref), tphy(NNref),phic(NNref),phie(NNref)
+   REAL(real64) gp(3), gw(3)
+   REAL(real64) Xcomputational,Xc,Xe,Ycomputational,Yc,Ye,dett
+
+   REAL(real64) ci,cx,cy
+   INTEGER i,j,k,l,m,n
+   
+
+
+   gw  =(/0.27777777777778, 0.444444444444, 0.27777777777778/)
+   gp =(/0.1127016654    , 0.5           , 0.8872983346    /)
+
+   DO i = 1,NNref
+      ngl(i) = nop(NEL,i)
+   ENDDO
+
+   phi=0.
+   phic=0.
+   phie=0.
+   tphx=0.
+   tphy=0.
+   ! Loop over qauss points
+   DO j = 1,3
+      DO k = 1,3
+
+      call TestFunctions(gp(j),gp(k),phi,phic,phie)
+
+      ! Defines the computational domain coordinates and the 2-dimensional Jacobian dett
+      Xcomputational=0.
+      Xc=0.
+      Xe=0.
+      Ycomputational=0.
+      Yc=0.
+      Ye=0.
+      DO n=1,NNref
+         Xcomputational= Xcomputational + x(ngl(n)) * phi(n)
+         Xc= Xc + x(ngl(n)) * phic(n)
+         Xe= Xe + x(ngl(n)) * phie(n)
+         Ycomputational= Ycomputational + y(ngl(n)) * phi(n)
+         Yc= Yc + y(ngl(n)) * phic(n)
+         Ye= Ye + y(ngl(n)) * phie(n)
+      ENDDO
+      dett=Xc*Ye-Xe*Yc
+
+      DO i=1,NNref
+         tphx(i)=(Ye*phic(i)-Yc*phie(i))/dett
+         tphy(i)=(Xc*phie(i)-Xe*phic(i))/dett
+      ENDDO
+
+
+      ci = 0.
+      cx = 0.
+      cy = 0.
+      DO i=1,NNref
+         ci = ci + c(ngl(i),timePoint)*phi(i)
+         cx = cx + c(ngl(i),timePoint)*tphx(i)
+         cy = cy + c(ngl(i),timePoint)*tphy(i)
+      ENDDO
+
+      DO l=1,NNref
+         DO m=1,NNref
+            P(ngl(l),timepoint) = P(ngl(l),timepoint) + gw(j)*gw(k)*dett*(ci**3 - ci)*phi(l)
+            DP(ngl(l),ngl(m),timepoint) =  DP(ngl(l),ngl(m),timepoint) &
+               +gw(j)*gw(k)*dett*(3.*(ci**2) - 1.)*phi(l)*phi(m)
+         ENDDO
+      ENDDO
+
+   ENDDO
+ENDDO
+
+
+END SUBROUTINE NonlinearTerm
+
+!>******************************************************************************
+!> Constructs the stationary system of each Newton procedure cycle
+!> .
+!> -------------
+!> Params:
+!> -------------
+!> timepoint:           the current time point
+!> c:                   Order parameter
+!> w:                   Chemical Potential
+!> STIFF,MASS:          System matrices
+!> P:                   Non-linear term of the weak formulation
+!> DP:                  Derivative of the non-linear term of the weak formulation
+!> fjac:                jacobian matrix of newton procedure
+!> fvec:                system of equations of newton procedure
+!>*********************************************************************************
+SUBROUTINE StationarySystem(timepoint,c,w,STIFF,MASS,P,DP,fjac,fvec)
+   use, intrinsic :: iso_fortran_env
+   IMPLICIT NONE
+   INCLUDE 'CHparameters.inc'
+
+   !> Current time step
+   INTEGER timepoint
+   !> Order parameter
+   REAL(real64) c(NNmax,Tmax)
+   !> Auxiliary variable-Chemical potential
+   REAL(real64) w(NNmax,Tmax)
+   !> System matrices
+   !> STIFF - Stiffness
+   !> MASS - Mass
+   REAL(real64) STIFF(NNmax,NNmax)
+   REAL(real64) MASS(NNmax,NNmax)
+   !> Non-linear term of the weak formulation
+   REAL(real64) P(NNmax,Tmax)
+   !> Derivative of the non-linear term of the weak formulation
+   REAL(real64) DP(NNmax,NNmax,Tmax)
+
+   !> Jacobian matix of Newton system
+   REAL(real64) fjac(2*NNmax,2*NNmax)
+   !> Righ handside vector of Newton system
+   REAL(real64) fvec(2*NNmax)
+
+   INTEGER i,j
+
+
+   DO i=1,NNmax
+      DO j=1,NNmax
+         fvec(i)= fvec(i) + MASS(i,j)*w(j,timepoint) - P(j,timepoint) - (epsilon)**2*STIFF(i,j)*c(j,timepoint)
+         fjac(i,j)= fjac(i,j) + MASS(i,j)
+      ENDDO
+
+      DO j= NNmax+1,2*NNmax
+         fjac(i,j)=  fjac(i,j) - DP(i,j-NNmax,timepoint) - (epsilon)**2*STIFF(i,j-NNmax)
+      ENDDO
+   ENDDO
+
+   DO i=NNmax+1,2*NNmax
+      DO j=1,NNmax
+         fvec(i)= fvec(i) + deltat*STIFF(i-NNmax,j)*w(j,timepoint) + MASS(i-NNmax,j)*c(j,timePoint) - MASS(i-NNmax,j)*c(j,timePoint-1)
+         fjac(i,j)= fjac(i,j) + deltat*STIFF(i-NNmax,j)
+      ENDDO
+      DO j= NNmax+1,2*NNmax
+         fjac(i,j)= fjac(i,j) + MASS(i-NNmax,j-NNmax)
+      ENDDO
+   ENDDO
+
+END SUBROUTINE
+
 
 
  !>******************************************************************************
@@ -380,9 +713,10 @@ END SUBROUTINE
  !> -------------
  !> Params:
  !> -------------
- !> point:               the point at which to calculate test functions
+ !> x,y:               the point at which to calculate test functions
  !> ph:                  array of test functions
- !> phd:                 array of derivatives for test functions
+ !> phic:                array of derivatives on c for test functions
+ !> phie:                array of derivatives on e for test functions
  !>******************************************************************************
 
 SUBROUTINE TestFunctions(x,y,phi,phic,phie)
---
+++ CahnHilliard/README.md
@@ -0,0 +1,17 @@
+# CahnHilliard
+
+## Program Structure
+
+![structure](./img/CahnHilliard_program_structure.svg)
+
+# Setting Parameters
+Change the parameters located under `/CahnHilliard/CHparameters.inc`
+
+# Running
+
+Make sure you are located in the projects root directory and run the below make command:
+
+```bash
+$ cd CahnHilliard
+$ make
+```
